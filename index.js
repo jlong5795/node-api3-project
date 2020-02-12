17a42f1f4714e21658fc4d5cd8097804b938c898
Jason Long <jlong5795@gmail.com>
2020-02-12 14:45:23 -0600

    Set up server and first logger

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code away!
+const server = require('./server.js');
+
+const port = 5000;
+
+server.listen(port, () => {
+    console.log(`\n Server is running on http://localhost:${port} *\n`);
+});
--- server.js
+++ server.js
@@ -1,13 +1,26 @@
+// module imports
 const express = require('express');
+const helmet = require('helmet');
+
+// file imports
+const Users = require('./users/userRouter.js');
+const Posts = require('./posts/postRouter.js');
 
 const server = express();
 
-server.get('/', (req, res) => {
+// global middleware
+server.use(express.json());
+server.use(helmet());
+
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+  next();
+};
 
 module.exports = server;

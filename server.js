c60ceea22b4a9650ca81d43df54cc9b0940f25ff
Jason Long <jlong5795@gmail.com>
2020-02-13 06:35:55 -0600

    Completed MVP and stretch goal.

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const express = require('express');
 const helmet = require('helmet');
 
 // file imports
-const Users = require('./users/userRouter.js');
-const Posts = require('./posts/postRouter.js');
+const userRouter = require('./users/userRouter.js');
+const postRouter = require('./posts/postRouter.js');
 
 const server = express();
 
 // global middleware
 server.use(express.json());
 server.use(helmet());
+
+server.use("/api/posts", logger, postRouter);
+server.use("/api/users", logger,  userRouter);
+
 
 server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -19,7 +23,7 @@ server.get('/', logger, (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url} endpoint.`);
   next();
 };
 
